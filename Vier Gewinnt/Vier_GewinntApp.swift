93e62d69326704406d388925e1ed665d5251262b
Firat <fsulunku@gmail.com>
2022-02-01 19:27:45 +0100

    Initial Commit

---
+++ Vier Gewinnt/Vier_GewinntApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vier_GewinntApp.swift
+//  Vier Gewinnt
+//
+//  Created by Firat Sülünkü on 01.02.22.
+//
+
+import SwiftUI
+
+@main
+struct Vier_GewinntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
