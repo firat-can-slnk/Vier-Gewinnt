4b9060c633e0ca66da8e8395c93e3acb9859c1f7
Firat <fsulunku@gmail.com>
2022-02-01 21:27:16 +0100

    Züge funktionieren

--- Vier Gewinnt/ContentView.swift
+++ Vier Gewinnt/ContentView.swift
@@ -8,12 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var gameField = GameModel().gameField
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView
+        {
+            HStack
+            {
+                ForEach(Array(zip(gameField.field.indices, gameField.field)), id: \.1.id)
+                {
+                    i, column in
+                    
+                    VStack
+                    {
+                        ForEach(Array(zip(column.indices, column)), id: \.1.id)
+                        {
+                            j, row in
+                            HStack
+                            {
+                                Button {
+                                    gameField.addMove(player: gameField.playersTurn, column: i)
+                                } label: {
+                                    stoneView(row)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    public func stoneView(_ stone: Stone) -> some View
+    {
+        let size = UIScreen.main.bounds.size.width / CGFloat(gameField.field.count) - 20
+        return Circle()
+            .foregroundColor(stone.color)
+            .frame(width: size, height: size, alignment: .center)
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
---
+++ Vier Gewinnt/GameModel.swift
@@ -0,0 +1,120 @@
+//
+//  GameModel.swift
+//  Vier Gewinnt
+//
+//  Created by Firat Sülünkü on 01.02.22.
+//
+
+import Foundation
+import SwiftUI
+
+public class GameModel
+{
+    let gameField: GameField = .init()
+}
+
+public class GameField: ObservableObject
+{
+    public static let rows = 6
+    public static let columns = 7
+    
+    internal init() {
+        
+        self.field = .init(repeating: .init(repeating: .init(player: .none), count: GameField.rows), count: GameField.columns)
+        
+        for column in 0..<GameField.columns {
+            for row in 0..<GameField.rows
+            {
+                self.field[column][row] = .init(player: .none)
+            }
+        }
+        
+    }
+    
+    
+    @Published var field: [[Stone]]
+    
+    @Published var playersTurn: Player = .one
+    
+    public func addMove(player: Player, column: Int)
+    {
+        if let lastCell = getLastCell(column: column)
+        {
+            print("col: \(lastCell.column); row: \(lastCell.row)")
+            if canMove(player: player, cell: lastCell)
+            {
+                field[lastCell.column][lastCell.row] = .init(player: player)
+                switchPlayer()
+            }
+        }
+    }
+    
+    public func canMove(player: Player, cell: (row: Int, column: Int)) -> Bool
+    {
+        return !isCellOccupied(cell) && playersTurn == player
+    }
+    
+    private func isCellOccupied(_ cell: (row: Int, column: Int)) -> Bool
+    {
+        return field[cell.column][cell.row].player.isPlayer
+    }
+    
+    private func getLastCell(column: Int) -> (row: Int, column: Int)?
+    {
+        let field = field[column].lastIndex { stone in
+            !stone.player.isPlayer
+        }
+        
+        return field != nil ? (row: field!, column: column) : nil
+    }
+    
+    private func switchPlayer()
+    {
+        if playersTurn == .one
+        {
+            playersTurn = .two
+        }
+        else if playersTurn == .two
+        {
+            playersTurn = .one
+        }
+        else
+        {
+            print("Who the fucks turn is it?")
+        }
+    }
+}
+
+extension Array: Identifiable where Element: Hashable {
+   public var id: Self { self }
+}
+
+public struct Stone: Identifiable, Hashable
+{
+    public var id: UUID = UUID()
+    var player: Player
+    
+    var color: Color
+    {
+        switch player
+        {
+            case .one:
+                return .red
+            case .two:
+                return .yellow
+            case .none:
+                return .gray
+        }
+    }
+}
+public enum Player
+{
+    case one
+    case two
+    case none
+    
+    var isPlayer: Bool
+    {
+        return self != .none
+    }
+}
